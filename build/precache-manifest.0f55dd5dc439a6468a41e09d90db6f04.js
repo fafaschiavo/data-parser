3ef27eff960d2ed63e4a0d8836dff38c59bd93b6
Fabricio Schiavo <fayschiavo@gmail.com>
2020-03-22 16:05:35 -0300

    localhost endpoint added to contasnts file

--- build/precache-manifest.0f55dd5dc439a6468a41e09d90db6f04.js
+++ build/precache-manifest.0f55dd5dc439a6468a41e09d90db6f04.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c31a13d0049cb3fc00c72d1d7c56404",
+    "revision": "996d62cb9ccbb867d98abccffeba71e8",
     "url": "/index.html"
   },
   {
-    "revision": "1c46100995f0c88cd0a8",
+    "revision": "de21befdaf0e5cd422b9",
     "url": "/static/css/main.c94e3641.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5f9a6abd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1c46100995f0c88cd0a8",
-    "url": "/static/js/main.ccad90ef.chunk.js"
+    "revision": "de21befdaf0e5cd422b9",
+    "url": "/static/js/main.549c5261.chunk.js"
   },
   {
     "revision": "8e7a1b01d83e662d704b",
