2655f990340507c9453d0ddd40ee20923c05ef30
Fabricio Schiavo <fayschiavo@gmail.com>
2020-03-22 15:38:28 -0300

    build folder removed from gitignore

---
+++ build/precache-manifest.b1a16513edce5ab0fbb8a36b18c99d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3caaa3cf17c871bc9a373956faa37a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f0ee948931fc5b7a9ce",
+    "url": "/static/css/main.c94e3641.chunk.css"
+  },
+  {
+    "revision": "33a394880bce2ab291a6",
+    "url": "/static/js/2.5f9a6abd.chunk.js"
+  },
+  {
+    "revision": "225b9d70ef2934503df3e1d5567e6bf5",
+    "url": "/static/js/2.5f9a6abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0ee948931fc5b7a9ce",
+    "url": "/static/js/main.26545b52.chunk.js"
+  },
+  {
+    "revision": "8e7a1b01d83e662d704b",
+    "url": "/static/js/runtime-main.b2d10935.js"
+  }
+]);
